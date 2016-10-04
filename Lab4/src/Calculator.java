840c6caa738f1dea27909b4a454166788d2fa631
HHKANG <khhcjswo212@naver.com>
2016-10-04 11:23:17 +0900

    finish

--- Lab4/src/Calculator.java
+++ Lab4/src/Calculator.java
@@ -1,41 +1,39 @@
-
 public class Calculator {
-	public Calculator
-	{
-		String name1 = "Jeon Joon Hyung";
-		String name2 = "Kang Jeong Kyu";
-		String name3 = "Kang Ho Hyung";
-	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("hi");
 	}
 	public double add(double x, double y)
 	{
-		double result=0;
-		result = x + y;
-		return result;
+		return 0;
 	}
 	public double subtract(double x, double y)
 	{
-		return -1;
+		double sub;
+		sub = x-y;
+		
+		return sub;
 	}
 	public double multiply(double x, double y)
 	{
-		double result=0;
-		result = x*y
-		return result;
+		return 0;
 	}
 	public double divide(double x, double y)
 	{
-		return 0;
+		double div;
+		div = x/y;
+		if(y == 0){
+			System.out.println("division can't zero");
+			return 0;
+		}
+		return div;
 	}
-	public toString toString()
-	{
-		return name1 + " " + name2 + " " + name3 + " " ID1 + " " 
-			   ID2 + " " ID3 + " " + URI;
+	public int remainder(double x, double y){
+		int rem;
+		rem = (int)(x%y);
+		
+		return rem;
 	}
-
 }
+
