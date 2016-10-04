9283b5c84e96138868ba6c6f67bae72d7c172c49
JeongQ <kutemarine@naver.com>
2016-10-04 10:56:44 +0900

    done create method

--- Lab4/src/Calculator.java
+++ Lab4/src/Calculator.java
@@ -5,5 +5,21 @@ public class Calculator {
 		// TODO Auto-generated method stub
 
 	}
+	public double add(double x, double y)
+	{
+		return 0;
+	}
+	public double subtract(double x, double y)
+	{
+		return 0;
+	}
+	public double multiply(double x, double y)
+	{
+		return 0;
+	}
+	public double divide(double x, double y)
+	{
+		return 0;
+	}
 
 }
