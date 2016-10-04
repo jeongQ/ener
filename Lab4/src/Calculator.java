e92e754a5411b9b64cd888789667d3a86836ef1f
HHKANG <khhcjswo212@naver.com>
2016-10-04 11:43:06 +0900

    hi

--- Lab4/src/Calculator.java
+++ Lab4/src/Calculator.java
@@ -1,50 +1,40 @@
-import java.util.*;
-
 public class Calculator {
-	
-	String name1;
-	String name2;
-	String name3;
-	String ID1;
-	String ID2;
-	String ID3;
-	String URI="https://github.com/jeongQ/ener.git";
-	public Calculator(String a1, String a2, String a3, String ID1, String ID2, String ID3, String URI)
-	{
-		a1 = "Jeon Joon Hyung";
-		a2 = "Kang Jeong Kyu";
-		a3 = "Kang Ho Hyung";
-		ID1 = "jeonjoonhyung";
-		ID2 = "HHKANG";
-		ID3 = "jeongQ";
-		URI = "https://github.com/jeongQ/ener.git";
-	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("hi");
 	}
 	public double add(double x, double y)
 	{
-		double result=0;
-		result = x + y;
-		return result;
+		return 0;
 	}
 	public double subtract(double x, double y)
 	{
-		return 0;
+		double sub;
+		sub = x-y;
+		
+		return sub;
 	}
 	public double multiply(double x, double y)
 	{
-		double result=0;
-		result = x*y;
-		return result;
+		return 0;
 	}
 	public double divide(double x, double y)
 	{
-		return 0;
+		double div;
+		if(y == 0){
+			System.out.println("division can't zero");
+			return 0;
+		}
+		div = x/y;
+		
+		return div;
 	}
-	
-
+	public int remainder(double x, double y){
+		int rem;
+		rem = (int)(x%y);
+		
+		return rem;
+	}
 }
+
